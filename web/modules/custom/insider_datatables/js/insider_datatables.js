363c30dcf971ee1392b69fdda09f91bab6735098
twerdal <thayne.werdal@gov.bc.ca>
2020-05-04 17:15:27 +0000

    Update to test class functionality and add in some id's

--- web/modules/custom/insider_datatables/js/insider_datatables.js
+++ web/modules/custom/insider_datatables/js/insider_datatables.js
@@ -16,7 +16,10 @@
   Drupal.behaviors.dataTables = {
     attach: function (context, drupalSettings) {
       $(document).ready( function () {
+        $('.agency-datatable').DataTable();
         $('#agency-datatable').DataTable();
+        $('#agency-datatable-2').DataTable();
+        $('#agency-datatable-3').DataTable();
       } );
     }
   };
