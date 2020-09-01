b9e8518346278009c34dd87b0ca64121e794973f
tvwerdal <thayne.werdal@gov.bc.ca>
2020-09-01 12:27:55 -0700

    Split out classes from constructor - added some documentation

---
+++ web/modules/custom/insider_events/src/InsiderEventsIcalFile.php
@@ -0,0 +1,113 @@
+<?php
+namespace Drupal\insider_events;
+
+use DateTime;
+use Drupal;
+use Drupal\Core\File\FileSystemInterface;
+use Exception;
+use RuntimeException;
+
+
+class InsiderEventsIcalFile {
+  protected $iCal_file;
+  protected $iCal_url;
+  protected $is_dir;
+  protected $timestamp;
+  protected $node_id;
+  protected $this_event;
+
+  public function __construct($iCal, $event){
+    $this->iCal_file = $iCal;
+    $this->timestamp = $this->setTimestamp();
+    $this->this_event = $event;
+    $this->create_directory();
+    $this->saveIcal();
+  }
+
+  /**
+   * @param $url
+   *   The variable that we will pass back to the event variable so we can create a download link.
+   */
+  private function setIcalUrl($url) {
+    $this->iCal_url = $url;
+  }
+
+  private function setTimestamp() {
+    $timestamp = new DateTime();
+    return $timestamp->getTimestamp();
+  }
+
+  protected function getTimestamp() {
+    return $this->timestamp;
+  }
+
+
+
+  /**
+   * @return mixed
+   *   Getter for the URL of the iCal file.
+   */
+  public function getIcalUrl() {
+    return $this->iCal_url;
+  }
+
+  /**
+   * Getter for any field we have set in the event object.
+   * @param null $field
+   *  The string indicating which field is requested. If no match, then return null.
+   * @return mixed|null
+   *  Return the matching event object field - if no match then return null.
+   */
+  protected function getEventField($field = null) {
+    $event = $this->getEvent();
+    return isset($event[$field]) ? $event[$field] : null;
+  }
+
+  /**
+   * @return mixed
+   *   Return the event array for parsing in various methods.
+   */
+  private function getEvent() {
+    return $this->this_event;
+  }
+
+  /**
+   *  Getter for Nid field.
+   */
+  private function getNid() {
+    $event = $this->getEvent();
+    return $event['event_nid'];
+  }
+
+  /**
+   * Helper function to save the iCal object as an iCal file.
+   */
+  private function saveIcal() {
+    // create a directory if it does not exist
+    $this->create_directory();
+    if($this->is_dir){
+      $this->setIcalUrl(file_save_data($this->getEventField('iCal'), 'public://ical/' . $this->getEventField('node_id') . '/' . str_replace(' ', '', $this->getEventField('event_title')) . '.ics', FileSystemInterface::EXISTS_REPLACE));
+    }
+  }
+
+  /**
+   * Helper function to help create the directory for the iCal file, based on node id.
+   */
+  private function create_directory() {
+    // We don't want multiple icals for every event- just one, so use node_id to separate these.
+    $new_dir = 'public://ical/' . $this->getNid();
+
+    Try{
+      $this->is_dir = Drupal::service('file_system')->prepareDirectory($new_dir, FileSystemInterface::CREATE_DIRECTORY);
+      if (!$this->is_dir) {
+        throw new RuntimeException("Could not create a directory for the file.");
+      }
+    }
+    catch (Exception $e) {
+      // Generic exception handling if something else gets thrown.
+      Drupal::logger('insider_event')->error($e->getMessage());
+    }
+
+  }
+
+}
--- web/modules/custom/insider_events/src/controller/InsiderEventsController.php
+++ web/modules/custom/insider_events/src/controller/InsiderEventsController.php
@@ -1,132 +1,54 @@
 <?php
 namespace Drupal\insider_events\Controller;
-use Liliumdev\ICalendar\ZCiCal; // Uses the iCalendar zapcalendar library - added via composer.
-use Liliumdev\ICalendar\ZCiCalDataNode;
-use Liliumdev\ICalendar\ZCiCalNode;
-use Liliumdev\ICalendar\ZCRecurringDate;
-use Liliumdev\ICalendar\ZCTimeZoneHelper;
-use Liliumdev\ICalendar\ZDateHelper;
+use Drupal;
+use Drupal\insider_events\iCalendarBuilder;
+use Drupal\insider_events\InsiderEventsIcalFile;
 
-class InsiderEventsController {
-  protected $this_event;
-  protected $iCal;
-  protected $iCal_file;
-  protected $iCal_url;
-  protected $is_dir;
-  protected $timestamp;
 
-  public function __construct($event){
-    $this->this_event = $this->setEvent($event);
-    $this->is_dir = 0;
-    $this->timestamp = date("m-d-Y");
-    $this->main();
-  }
+/**
+ * Class InsiderEventsController
+ * @package Drupal\insider_events\Controller
+ */
+class InsiderEventsController
+{
+  protected $ical_url;
+  private $ical_save;
 
-  public function main() {
-    // Create and ical text object.
-    $this->setIcal();
-    $this->saveIcal();
+  /**
+   * InsiderEventsController constructor.
+   * @param $event
+   *  $event param is an array passed from the event node. Contains:
+   *    - ['description']: The body field of the node with info about the event.
+   *    - ['event_title']: The title field of the node for the event.
+   *    - ['event_location']: The location field for the event node. Text only.
+   *    - ['event_online_link_uri']: URL location for any online meeting events.
+   *    - ['event_online_link_text']: The text to show that houses the link to any online meeting.
+   *    - ['event_nid']: The nid of the event node.
+   *    - ['recurring']: Set only if there are more than one date for this event. An array that contains rr rules and dates.
+   *    - ['ical_url']: The location of the new ical file.
+   *    - ['iCal_save']: The ical save location
+   */
+  public function __construct($event) {
+    $this->main($event);
   }
 
   /**
-   * Allow manual or auto setting of $event variable.
    * @param $event
-   * @return mixed
+   *   The event array passed from insider_events.module.
    */
-  public function setEvent($event) {
-    return $event;
+  public function main($event)
+  {
+    $ical = new iCalendarBuilder($event);
+    $ical_file = $ical->getIcal();
+    $this->ical_save = new InsiderEventsIcalFile($ical_file, $event);
+    $this->set_ical_url();
   }
 
-
-  private function setIcal() {
-    $tzid = "America/Vancouver";
-    $event = $this->getEvent();
-    $start_year = gmdate('Y', $event['start_datetime']);
-    $end_year = gmdate('Y', $event['end_datetime']);
-    // Create the object.
-    $icalobj = new ZCiCal();
-    ZCTimeZoneHelper::getTZNode($start_year,$end_year,$tzid, $icalobj->curnode);
-    // Create event within ical obj.
-    $eventobj = new ZCiCalNode("VEVENT", $icalobj->curnode);
-    // Set our timezone.
-    // add the title.
-    $eventobj->addNode(new ZCiCalDataNode("SUMMARY:" . $event['event_title']));
-    // Start date/time.
-    // Not sure if Uniq is a typo and will be fixed in any future updates.
-    // We must add the Z to the end to let it know this is a UTC timestamp - or else it assumes local.
-    $eventobj->addNode(new ZCiCalDataNode('DTSTART:' . ZDateHelper::fromUniqDateTimetoiCal($event['start_datetime']) . 'Z'));
-    // End date/time.
-    $eventobj->addNode(new ZCiCalDataNode('DTEND:' . ZDateHelper::fromUniqDateTimetoiCal($event['end_datetime']) . 'Z'));
-
-    // Create a unique UID for this. UID is required.
-    $uid = date('Y-m-d-H-i-s') .  "@bcpsa.gww.gov.bc.ca";
-    $eventobj->addNode(new ZCiCalDataNode("UID:" . $uid));
-    // DTSTAMP is required.
-    $eventobj->addNode(new ZCiCalDataNode("DTSTAMP:" . ZDateHelper::fromUniqDateTimetoiCal(time())));
-    // Description.
-    $eventobj->addNode(new ZCiCalDataNode("Description:" . ZCiCal::formatContent(
-        $event['description']
-      )));
-
-    // write iCalendar feed to stdout
-    $this->iCal = $icalobj->export();
-
+  private function set_ical_url() {
+    $this->ical_url = $this->ical_save->getIcalUrl();
   }
 
-  private function setIcalUrl($url) {
-    $this->iCal_url = $url;
+  public function get_ical_url() {
+    return $this->ical_url;
   }
-
-  private function getEvent() {
-    return $this->this_event;
-  }
-
-
-  public function getIcalFile() {
-    return $this->iCal_file;
-  }
-
-  public function getIcalUrl() {
-    return $this->iCal_url;
-  }
-
-  public function getIcal() {
-    // For testing
-    return $this->iCal;
-  }
-
-  private function saveIcal() {
-    // create a directory if it does not exist
-    $this->create_directory();
-    if($this->is_dir){
-      $event_name = $this->getEvent();
-      $this->iCal_url = file_save_data($this->iCal, 'public://ical/' . $this->timestamp . '/' . str_replace(' ', '', $event_name['event_title']) . '.ics');
-    }
-    // Create a file name and make sure it does not exist.
-    // save as an .ical file.
-    // return the .ical file location.
-  }
-
-  private function create_directory() {
-    $new_dir = 'public://ical/' . $this->timestamp;
-
-    Try{
-      $this->is_dir = file_prepare_directory($new_dir, FILE_CREATE_DIRECTORY);
-      if (!$this->is_dir) {
-        throw new \exception("Could not create a directory for the file.");
-      }
-    }
-    catch (Exception $e) {
-      // Generic exception handling if something else gets thrown.
-      \Drupal::logger('insider_event')->error($e->getMessage());
-    }
-
-  }
-
-
-
 }
-
-
-
-
