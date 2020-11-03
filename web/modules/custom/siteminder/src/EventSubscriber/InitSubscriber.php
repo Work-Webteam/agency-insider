b463fc42570e2e25f12e795145227a37cfac606c
twerdal <thayne.werdal@gov.bc.ca>
2020-11-03 11:23:36 -0800

    quick update to fix url stuff - again

--- web/modules/custom/siteminder/src/EventSubscriber/InitSubscriber.php
+++ web/modules/custom/siteminder/src/EventSubscriber/InitSubscriber.php
@@ -33,10 +33,16 @@ class InitSubscriber implements EventSubscriberInterface {
       $current_path = \Drupal::service('path.current')->getPath();
       // Check if a new user wants to edit his profile.
       $new_user = ($event->getRequest()->query->get('status') == "new_user") ? true : false;
-      if ($current_path != '/siteminder_login' && $current_path != '/user/login' && $current_path != '/access_denied' && $current_path != '/pending_validation' && $current_path != '/user/logout' && !$new_user) {
+      if ($current_path != '/siteminder_login' && $current_path != '/user/login' && $current_path != '/user/logout') {
         // If the user is NOT already logged in and the
         // HTTP header is sending a siteminder ID, login.
         if (!\Drupal::currentUser()->isAuthenticated()) {
+          // We want to save the original URL
+          // so that we can redirect users after login.
+          $config = \Drupal::service('config.factory')->getEditable('siteminder.settings');
+          if ($config->get('user_initial_url') == NULL) {
+            $config->set('user_initial_url', $current_path)->save();
+          }
           // Now that we have the URL, redirect to login.
           $response = new RedirectResponse("/siteminder_login", 301);
           $event->setResponse($response);
@@ -48,10 +54,12 @@ class InitSubscriber implements EventSubscriberInterface {
   /**
    * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    */
+  /*
   public function saveCurrentUrl(GetResponseEvent $event) {
     // If the user is NOT already logged in and the
     // HTTP header is sending a siteminder ID, login.
     if (!\Drupal::currentUser()->isAuthenticated()) {
+      \Drupal::service('page_cache_kill_switch')->trigger();
       $current_path = \Drupal::service('path.current')->getPath();
       if ($current_path != '/siteminder_login' && $current_path != '/user/login' && $current_path != '/user/logout') {
         // We want to save the original URL
@@ -62,12 +70,12 @@ class InitSubscriber implements EventSubscriberInterface {
       }
     }
   }
-
+*/
   /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = ['saveCurrentUrl', 28];
+    //$events[KernelEvents::REQUEST][] = ['saveCurrentUrl', 28];
     $events[KernelEvents::REQUEST][] = ['loginSiteminder', 100];
     return $events;
   }
