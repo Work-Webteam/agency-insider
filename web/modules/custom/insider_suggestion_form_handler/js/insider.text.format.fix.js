3670adf28f550ec8b94806b6096919d093969294
twerdal <thayne.werdal@gov.bc.ca>
2021-04-26 15:54:37 -0700

    Adding in jquery library for webform

---
+++ web/modules/custom/insider_suggestion_form_handler/js/insider.text.format.fix.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * JavaScript behaviors for CKeditor textarea boxes.
+ *   We want description to show after label.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Filter webform autocomplete handler.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.insiderTextFormatFix = {
+    attach: function (context) {
+      $('div.js-text-format-wrapper.text-format-wrapper.js-form-item.form-item .js-form-type-textarea .form-textarea-wrapper', context).once('moveDescription').each(function () {
+        // For each ckeditor block, locate the closest description div and append it below the label.
+        $('.js-text-format-wrapper.text-format-wrapper.js-form-item.form-item > .description').each(function () {
+          $(this).siblings('fieldset').children('.form-textarea-wrapper').prepend($(this));
+        });
+      });
+    }
+  };
+
+})(jQuery, Drupal);
